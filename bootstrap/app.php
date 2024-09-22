55759e7537799f8073f813ea35441c581aabf449
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-09-22 15:29:18 +0200

    Disabled health route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: null,
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(function (Request $request): string {
