1210137ea62d522124cf708ab0cf25be379f2dd0
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-09-27 22:58:32 +0200

    Basic methods added

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -3,31 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Response as InertiaView;
 
 class UserProfileController extends Controller
 {
     /**
      * Display the specified user.
      */
-    public function edit(User $user)
+    public function edit(User $user): InertiaView
     {
-        //
+        return inertia('CP/UserProfile/Edit');
     }
     
     /**
      * Update the specified user in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, User $user): RedirectResponse
     {
-        //
+        return back();
     }
 
     /**
      * Remove the specified user from storage.
      */
-    public function destroy(User $user)
+    public function destroy(User $user): RedirectResponse
     {
-        //
+        return back();
     }
 }
