e7bceb561635e4f7fe7e87f27822d5063f18e04e
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-09-27 22:55:52 +0200

    Added custom color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors";
+
 export default {
     content: [
         "./resources/**/*.blade.php",
@@ -7,6 +10,18 @@ export default {
     ],
     theme: {
         extend: {},
+        colors: {
+            primary: colors.emerald,
+            neutral: colors.stone,
+            accent: colors.orange,
+            "action-primary": colors.cyan,
+            "action-secondary": colors.purple,
+            impartial: colors.neutral,
+            info: colors.sky,
+            success: colors.lime,
+            warning: colors.amber,
+            alert: colors.red,
+        },
     },
     plugins: [],
 };
